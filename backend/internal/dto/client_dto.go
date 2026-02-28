6534c955bcf64efe3bde88027a608f77497a96bc
causon-mikolorenz <109595939+causon-mikolorenz@users.noreply.github.com>
2026-02-28 12:23:27 +0800

    Feat/dto (#6)
    * create generic response for error and success messages
    
    * add client data access object
    
    * create role data access object
    
    * create user data access object
    
    * create auth data access object

---
+++ backend/internal/dto/client_dto.go
@@ -0,0 +1,20 @@
+package dto
+
+type ClientResponse struct {
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	Abbreviation  string `json:"abbreviation"`
+	Description   string `json:"description"`
+	ImageLocation string `json:"image_location"`
+	BaseURL       string `json:"base_url"`
+	RedirectURI   string `json:"redirect_uri"`
+	LogoutURI     string `json:"logout_uri"`
+	CreatedAt     string `json:"created_at"`
+}
+
+type ClientListResponse struct {
+	Clients []ClientResponse `json:"clients"`
+	TotalCount     int              `json:"total_count"`
+	CurrentPage    int              `json:"current_page"`
+	LastPage       int              `json:"last_page"`
+}
