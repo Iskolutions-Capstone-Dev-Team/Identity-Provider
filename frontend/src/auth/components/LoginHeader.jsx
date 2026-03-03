94b2ac119f1e7ae52e4a7bce5100010cbdd9b7b9
efondo-aaronkyle <akyleefondo@gmail.com>
2026-03-03 09:26:56 +0800

    Feature/auth login (#11)
    * update gitignore
    
    * add index.html
    
    * add dependencies
    
    * add vite config
    
    * add app.jsx
    
    * add app.css
    
    * add index.css
    
    * add main.jsx
    
    * add axios service
    
    * add project images
    
    * add logo properties
    
    * add float logo property
    
    * add callback and unauthorized route
    
    * add login footer
    
    * add login form
    
    * add login header
    
    * add protected route
    
    * add auth layout
    
    * add callback
    
    * add login
    
    * add unauthorized
    
    * add auth service
    
    * add error alert
    
    * add token login
    
    * add privary policy
    
    * add terms of service
    
    * add frontend docker file
    
    * udpate env example
    
    * update main.go
    
    * update backend docker file
    
    * add frontend and fix minio
    
    * add s3 initialization on main
    
    * update gitignore
    
    ---------
    
    Co-authored-by: causon-mikolorenz <causonmikolorenz@gmail.com>

---
+++ frontend/src/auth/components/LoginHeader.jsx
@@ -0,0 +1,22 @@
+export default function LoginHeader() {
+  return (
+    <nav className="navbar bg-[#991b1b] shadow-sm h-24 sm:h-24 py-25 sm:py-0 px-4 sm:px-6 flex items-center">
+      <div className="flex flex-col sm:flex-row items-center sm:items-center gap-1 sm:gap-[1.2em] w-full">
+        <img
+          src="/assets/images/PUP_Logo.png"
+          alt="PUP Logo"
+          className="h-12 sm:h-15 object-contain"
+        />
+
+        <div className="flex flex-col text-white items-center sm:items-start text-center sm:text-left">
+          <div className="text-lg sm:text-[1.1em] font-bold tracking-[1px]">
+            PUP TAGUIG IDENTITY PROVIDER
+          </div>
+          <div className="text-sm sm:text-[.75em] opacity-[.9] mt-1 sm:mt-0">
+            POLYTECHNIC UNIVERSITY OF THE PHILIPPINES — TAGUIG CAMPUS
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
