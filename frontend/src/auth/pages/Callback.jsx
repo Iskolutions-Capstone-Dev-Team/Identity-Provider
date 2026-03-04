106cc9075de446eb3f4a8aafb37eaf670c39b591
efondo-aaronkyle <akyleefondo@gmail.com>
2026-03-04 15:49:42 +0800

    fix callback loading design

--- frontend/src/auth/pages/Callback.jsx
+++ frontend/src/auth/pages/Callback.jsx
@@ -1,12 +1,26 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { authService } from "../services/authService";
 
 export default function Callback() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/dashboard");
+    const validateSession = async () => {
+      try {
+        await authService.checkSession();
+        navigate("/roles");
+      } catch (err) {
+        navigate("/401");
+      }
+    };
+
+    validateSession();
   }, [navigate]);
 
-  return null;
+  return(
+    <div className="min-h-screen bg-[#991b1b] flex flex-col items-center justify-center text-white">
+      <img src="/assets/images/IDP_Logo.png" alt="IDP Logo" className="w-32 md:w-50 float-logo"/>
+    </div>
+  );
 }
