664de6bf53bbd1850679565547f59d5f6b5caea7
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-03-08 21:51:50 +0800

    update jwt generation

--- backend/internal/auth/jwt.go
+++ backend/internal/auth/jwt.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/Iskolutions-Capstone-Dev-Team/Identity-Provider/internal/models"
@@ -18,24 +19,23 @@ func GenerateToken(privateKey *rsa.PrivateKey,
 ) (string, error) {
 	now := time.Now()
 
-	// 1. Convert the binary UUID to a standard hex string for the 'aud' claim.
 	clientIDStr := hex.EncodeToString(clientID)
 
-	// 2. Set the standard OIDC registered claims
 	claims.RegisteredClaims = jwt.RegisteredClaims{
-		Subject:   claims.ID, // Assuming this is the User's unique string ID
-		Issuer:    "unified-access-idp",
+		Subject:   claims.ID,
+		Issuer:    "pupt-idp",
 		Audience:  jwt.ClaimStrings{clientIDStr},
 		ExpiresAt: jwt.NewNumericDate(now.Add(1 * time.Hour)),
 		IssuedAt:  jwt.NewNumericDate(now),
 		NotBefore: jwt.NewNumericDate(now),
 		ID:        fmt.Sprintf("%d", now.UnixNano()),
 	}
+	
 
-	// 3. Create the token using RS256 (Asymmetric)
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
 
-	// 4. Sign the token using the RSA Private Key
+	token.Header["kid"] = os.Getenv("KEY_ID")
+
 	signedToken, err := token.SignedString(privateKey)
 	if err != nil {
 		return "", fmt.Errorf("failed to sign JWT: %w", err)
