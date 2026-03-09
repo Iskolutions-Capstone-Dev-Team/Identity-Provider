7213f9f63fe8237c7df83505d80991a9a2b9ebd5
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-03-10 07:44:09 +0800

    move auth technical functions to utils

---
+++ backend/internal/utils/crypto.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GenerateAuthorizationCode() (string, error) {
+	b := make([]byte, 32)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate secure bytes: %w", err)
+	}
+
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
+
+func HashSecret(plainText string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(plainText), 12)
+	return string(bytes), err
+}
+
+func CompareSecret(hashed, plain string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashed), []byte(plain))
+}
+
+func GenerateRandomString(length int) (string, error) {
+    b := make([]byte, length)
+    
+    if _, err := rand.Read(b); err != nil {
+        return "", err
+    }
+
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
