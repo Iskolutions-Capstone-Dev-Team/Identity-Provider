37c2b00db617589d5c9bc97ffae324736a7d9977
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-03-10 12:33:36 +0800

    create servicecontainer struct

---
+++ backend/internal/service/service.go
@@ -0,0 +1,9 @@
+package service
+
+type ServiceContainer struct {
+	ClientService *ClientService
+	// RoleService *RoleService
+	// UserService *UserService
+	// AuthService *AuthService
+	// TokenService *TokenService	
+}
