2dcc1fc4ca158352a62b488a5c184e41d0f1cd69
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-03-10 12:36:38 +0800

    add constants file for services to use

---
+++ backend/internal/service/constants.go
@@ -0,0 +1,13 @@
+package service
+
+const (
+	PAGE_LIMIT         = 10
+	TIME_LAYOUT        = "2006-01-02 15:04:05"
+	SESSION_YEARS      = 0
+	SESSION_MONTHS     = 0
+	SESSION_DAYS       = 15
+	HEADER_SIZE        = 512
+	ACCESS_TOKEN_NAME  = "access_token"
+	REFRESH_TOKEN_NAME = "refresh_token"
+	SECRET_ENTROPY     = 32
+)
