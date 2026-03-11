74d8df1a4513a067d8f853b61922bc40e77c61b7
efondo-aaronkyle <akyleefondo@gmail.com>
2026-03-11 14:00:14 +0800

    fix navbar endpoint

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar({ sidebarOpen }) {
         </div>
       </div>
       <div className="flex items-center gap-2 sm:gap-4">
-        <button onClick={() => navigate("/idp/profile")} className="group relative flex items-center gap-2 md:gap-3 px-2 md:px-4 py-2 md:py-3 rounded-lg sm:rounded-xl bg-white/10 backdrop-blur-sm border border-white/10 transition-all duration-300 hover:bg-white/20 hover:scale-105 hover:shadow-lg hover:shadow-black/30 active:scale-95">
+        <button onClick={() => navigate("/profile")} className="group relative flex items-center gap-2 md:gap-3 px-2 md:px-4 py-2 md:py-3 rounded-lg sm:rounded-xl bg-white/10 backdrop-blur-sm border border-white/10 transition-all duration-300 hover:bg-white/20 hover:scale-105 hover:shadow-lg hover:shadow-black/30 active:scale-95">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 sm:w-11 sm:h-11 text-white transition-transform duration-300 group-hover:scale-110">
             <path fillRule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-5.5-2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10 12a5.99 5.99 0 0 0-4.793 2.39A6.483 6.483 0 0 0 10 16.5a6.483 6.483 0 0 0 4.793-2.11A5.99 5.99 0 0 0 10 12Z" clipRule="evenodd" />
           </svg>
