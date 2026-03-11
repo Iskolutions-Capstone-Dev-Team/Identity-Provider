172cf3aaef516001e0b1959e12b580dcb5e856bf
efondo-aaronkyle <akyleefondo@gmail.com>
2026-03-12 01:46:52 +0800

    fix icon in page header

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -53,17 +53,8 @@ export default function Profile() {
         title="Profile"
         description="View and manage your account details"
         icon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-28 h-28 text-[#991b1b]"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-7 9a7 7 0 1 1 14 0H3Z"
-              clipRule="evenodd"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-28 h-28 text-[#991b1b]">
+            <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
           </svg>
         }
       />
