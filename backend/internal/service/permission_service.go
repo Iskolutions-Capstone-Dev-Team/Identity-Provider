56ca94cb303a2c2f8ef0cc0f0988b819d0b45c52
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-03-27 20:19:50 +0800

    create permission service

---
+++ backend/internal/service/permission_service.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"github.com/Iskolutions-Capstone-Dev-Team/Identity-Provider/internal/dto"
+	"github.com/Iskolutions-Capstone-Dev-Team/Identity-Provider/internal/repository"
+)
+
+type PermissionService struct {
+	Repo *repository.PermissionRepository
+}
+
+func (s *PermissionService) GetAllPermissions() ([]dto.PermissionResponse, 
+	error) {
+	permissions, err := s.Repo.GetAllPermissions()
+	if err != nil {
+		return nil, err
+	}
+
+	var response []dto.PermissionResponse
+	for _, p := range permissions {
+		response = append(response, dto.PermissionResponse{
+			ID:         p.ID,
+			Permission: p.PermissionName,
+		})
+	}
+	return response, nil
+}
