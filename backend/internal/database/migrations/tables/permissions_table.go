9d13e2bce20c9eb66679456bd1540b4b99d2daaf
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-03-27 20:35:11 +0800

    add default permissions to permissions table migration

--- backend/internal/database/migrations/tables/permissions_table.go
+++ backend/internal/database/migrations/tables/permissions_table.go
@@ -12,5 +12,26 @@ var PermissionsMigration = migrations.TableMigration{
 				permission VARCHAR(100) NOT NULL UNIQUE
 			);`,
 		},
+		{
+			ID: "add-default-permissions",
+			SQL: `INSERT IGNORE INTO permissions (permission) VALUES 
+				('Add user'),
+				('Edit user'),
+				('Delete user'),
+				('View all users'),
+				('View users based on appclient'),
+				('Approve registration request'),
+				('Reject registration request'),
+				('View registration requests'),
+				('Add appclient'),
+				('Edit appclient'),
+				('Delete appclient'),
+				('View all appclients'),
+				('View roles'),
+				('Assign Roles'),
+				('Edit Roles'),
+				('Delete Roles'),
+			;`,
+		},
 	},
 }
