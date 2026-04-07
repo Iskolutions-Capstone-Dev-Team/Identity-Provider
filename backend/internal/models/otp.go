b8af82f5e30772103ea67484a2154ea75448f83a
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-04-07 22:15:59 +0800

    create models for schemas created

---
+++ backend/internal/models/otp.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+)
+
+type OTP struct {
+	OTP       string     `db:"otp"`
+	UserID    []byte     `db:"user_id"`
+	ExpiresAt time.Time  `db:"expires_at"`
+	UsedAt    *time.Time `db:"used_at"`
+	CreatedAt time.Time  `db:"created_at"`
+}
