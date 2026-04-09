d7225a117385e7348ac4b1a3c60053f5d7e43e7f
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-04-09 21:03:59 +0800

    use email in otp model

--- backend/internal/models/otp.go
+++ backend/internal/models/otp.go
@@ -6,7 +6,7 @@ import (
 
 type OTP struct {
 	OTP       string     `db:"otp"`
-	UserID    []byte     `db:"user_id"`
+	Email     string     `db:"email"`
 	ExpiresAt time.Time  `db:"expires_at"`
 	UsedAt    *time.Time `db:"used_at"`
 	Attempts  int        `db:"attempts"`
