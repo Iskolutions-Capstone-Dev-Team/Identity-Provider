50e013130c92bd5f1c9e200977b07642441b322b
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-04-14 18:31:28 +0800

    update invitation code model structure

--- backend/internal/models/invitation.go
+++ backend/internal/models/invitation.go
@@ -2,19 +2,10 @@ package models
 
 import "time"
 
-type InvitationType string
-
-const (
-	InvitationStudent   InvitationType = "student"
-	InvitationAdmin     InvitationType = "admin"
-	InvitationGuest     InvitationType = "guest"
-	InvitationApplicant InvitationType = "applicant"
-)
-
 type InvitationCode struct {
-	ID             int            `db:"id"`
-	Email          string         `db:"email"`
-	InvitationType InvitationType `db:"invitation_type"`
-	InvitationCode string         `db:"invitation_code"`
-	CreatedAt      time.Time      `db:"created_at"`
+	ID             int       `db:"id"`
+	Email          string    `db:"email"`
+	AccountTypeID  int       `db:"account_type_id"`
+	InvitationCode string    `db:"invitation_code"`
+	CreatedAt      time.Time `db:"created_at"`
 }
