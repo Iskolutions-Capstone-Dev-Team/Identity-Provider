243c894cc6e264849c322c01c758580395b4ac82
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-05-15 00:12:24 +0800

    Fix AuthenticatorMetadata mapping by adding db tags and binary ID

--- backend/internal/models/user_authenticator.go
+++ backend/internal/models/user_authenticator.go
@@ -5,16 +5,16 @@ import (
 )
 
 type UserAuthenticator struct {
-	ID               []byte     `db:"id" json:"id"`
-	UserID           []byte     `db:"user_id" json:"user_id"`
-	Type             string     `db:"type" json:"type"`
-	Name             string     `db:"name" json:"name"`
-	CreatedAt        time.Time  `db:"created_at" json:"created_at"`
-	LastUsedAt       *time.Time `db:"last_used_at" json:"last_used_at"`
-	SecretEncrypted  []byte     `db:"secret_encrypted" json:"-"`
-	CredentialID     []byte     `db:"credential_id" json:"-"`
-	PublicKey        []byte     `db:"public_key" json:"-"`
-	SignCount        int        `db:"sign_count" json:"sign_count"`
+	ID              []byte     `db:"id" json:"id"`
+	UserID          []byte     `db:"user_id" json:"user_id"`
+	Type            string     `db:"type" json:"type"`
+	Name            string     `db:"name" json:"name"`
+	CreatedAt       time.Time  `db:"created_at" json:"created_at"`
+	LastUsedAt      *time.Time `db:"last_used_at" json:"last_used_at"`
+	SecretEncrypted []byte     `db:"secret_encrypted" json:"-"`
+	CredentialID    []byte     `db:"credential_id" json:"-"`
+	PublicKey       []byte     `db:"public_key" json:"-"`
+	SignCount       int        `db:"sign_count" json:"sign_count"`
 }
 
 type AuthenticatorMetadata struct {
