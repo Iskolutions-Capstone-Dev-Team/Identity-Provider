90c9d7c011d0b14c656e3558fc846dfc07d469d3
efondo-aaronkyle <akyleefondo@gmail.com>
2026-06-15 14:17:20 +0800

    add dashboard button

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,6 +9,23 @@ import { AppClientOutlineIcon } from "./app-client/AppClientIconBox";
 
 const menuSections = [
   {
+    title: "DASHBOARD",
+    items: [
+      {
+        name: "Dashboard",
+        tooltipLabel: "Dashboard",
+        path: "/dashboard",
+        requiredPermissions: [],
+        icon: ({ className }) => (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+          </svg>
+        ),
+      },
+    ],
+  },
+  {
     title: "IDENTITY MANAGEMENT",
     items: [
       {
