eaad0711c33239078492eac6c438d9cbaa148f84
efondo-aaronkyle <akyleefondo@gmail.com>
2026-06-26 19:06:47 +0800

    isolate app client icons

--- frontend/src/features/app-clients/components/ConnectedAppClientTable.jsx
+++ frontend/src/features/app-clients/components/ConnectedAppClientTable.jsx
@@ -1,6 +1,7 @@
 import TableRowFade from "../../../components/TableRowFade";
 import DataTableSkeleton from "../../../components/DataTableSkeleton";
 import EmptySearchState from "../../../components/EmptySearchState";
+import { DeleteIcon, EditIcon, RotateSecretIcon, ViewIcon } from "./appClientIcons";
 
 const getClientId = (client) => client?.id ?? client?.clientId ?? "";
 
@@ -147,11 +148,7 @@ export default function ConnectedAppClientTable({ loading = false, clients, onVi
                           name: client.name,
                         }),
                       className: actionButtonClassName,
-                      children: (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
-                        </svg>
-                      ),
+                      children: (<RotateSecretIcon />),
                     })
                   ) : (
                     <span className={isDarkMode ? "text-[#a58d95]" : "text-[#8f6f76]"}>
@@ -166,12 +163,7 @@ export default function ConnectedAppClientTable({ loading = false, clients, onVi
                       label: `View ${client.name}`,
                       onClick: () => onView?.(client),
                       className: actionButtonClassName,
-                      children: (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"/>
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
-                        </svg>
-                      ),
+                      children: (<ViewIcon />),
                     })}
 
                     {showEditAction &&
@@ -179,11 +171,7 @@ export default function ConnectedAppClientTable({ loading = false, clients, onVi
                         label: `Edit ${client.name}`,
                         onClick: () => onEdit?.(client),
                         className: actionButtonClassName,
-                        children: (
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"/>
-                          </svg>
-                        ),
+                        children: (<EditIcon />),
                       })}
 
                     {showDeleteAction &&
@@ -191,11 +179,7 @@ export default function ConnectedAppClientTable({ loading = false, clients, onVi
                         label: `Delete ${client.name}`,
                         onClick: () => onDelete?.(getClientId(client)),
                         className: actionButtonClassName,
-                        children: (
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.108 0 0 0-7.5 0"/>
-                          </svg>
-                        ),
+                        children: (<DeleteIcon />),
                       })}
                   </div>
                 </td>
