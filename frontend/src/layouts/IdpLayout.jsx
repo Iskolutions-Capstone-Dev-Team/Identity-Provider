93945ae4e98250565c4a4e5d54ea55f405cbb028
efondo-aaronkyle <akyleefondo@gmail.com>
2026-07-04 22:07:09 +0800

    remove unused import

--- frontend/src/layouts/IdpLayout.jsx
+++ frontend/src/layouts/IdpLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useOutlet } from "react-router-dom";
 import AssistiveFab from "../components/AssistiveFab";
 import AccessibilityWidget from "../components/AccessibilityWidget";
