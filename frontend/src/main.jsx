4fb673f112d1a2b898d29f357175a8c5c956387c
efondo-aaronkyle <akyleefondo@gmail.com>
2026-07-09 23:41:31 +0800

    add components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { clearLegacyRefreshTokenCookie } from "./auth/utils/authCookies";
 import "./index.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
 
 clearLegacyRefreshTokenCookie();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+      <Toaster />
+    </TooltipProvider>
   </React.StrictMode>
 );
