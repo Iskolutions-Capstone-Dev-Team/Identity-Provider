86575a936b31488ae5f12942af9fe30f5efaa392
causon-mikolorenz <causonmikolorenz@gmail.com>
2026-07-12 18:26:04 +0800

    Implement null account type update in preapproved_clients_table

--- backend/internal/database/migrations/tables/preapproved_clients_table.go
+++ backend/internal/database/migrations/tables/preapproved_clients_table.go
@@ -16,5 +16,23 @@ var PreapprovedClientsMigration = migrations.TableMigration{
 				FOREIGN KEY (client_id) REFERENCES clients(id)
 			);`,
 		},
+		{
+			ID: "backfill-null-user-account-types",
+			SQL: `
+			UPDATE users u
+			JOIN client_allowed_users cau 
+				ON u.id = cau.user_id
+			JOIN preapproved_clients pc 
+				ON cau.client_id = pc.client_id
+			LEFT JOIN clients c 
+				ON pc.client_id = c.id
+			LEFT JOIN account_types sa 
+				ON sa.name = 'System Administrator'
+			SET u.account_type_id = CASE 
+				WHEN c.description = 'Identity Provider' THEN sa.id
+				ELSE pc.account_type_id 
+			END
+			WHERE u.account_type_id IS NULL;`,
+		},
 	},
 }
