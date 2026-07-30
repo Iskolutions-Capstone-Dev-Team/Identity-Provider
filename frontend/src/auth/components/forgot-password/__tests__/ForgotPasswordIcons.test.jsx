8f94e0802c056350a8ca5afa30ccbe02d4ae1bb5
efondo-aaronkyle <akyleefondo@gmail.com>
2026-07-30 19:14:31 +0800

    remove unused forgot password icons file

--- frontend/src/auth/components/forgot-password/__tests__/ForgotPasswordIcons.test.jsx
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import * as Icons from '../ForgotPasswordIcons';
-
-describe('ForgotPasswordIcons', () => {
-  it('exports icon components', () => {
-    expect(Object.keys(Icons).length).toBeGreaterThan(0);
-  });
-});
