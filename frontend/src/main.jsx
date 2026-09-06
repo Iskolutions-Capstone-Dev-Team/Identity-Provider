abdb33e245311b3ca7381dbe8a10e4e292e5c586
efondo-aaronkyle <akyleefondo@gmail.com>
2026-09-06 22:42:06 +0800

    install tanstack

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from "./App.jsx";
 import { clearLegacyRefreshTokenCookie } from "./auth/utils/authCookies";
 import "./index.css";
@@ -8,11 +10,23 @@ import { Toaster } from "@/components/ui/sonner";
 
 clearLegacyRefreshTokenCookie();
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TooltipProvider>
-      <App />
-      <Toaster position="top-right" />
-    </TooltipProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <App />
+        <Toaster position="top-right" />
+      </TooltipProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
