7f224d32b3dd0c0ee527f1e62ab4c0c173ef48dd
efondo-aaronkyle <akyleefondo@gmail.com>
2026-09-08 11:31:14 +0800

    Fix/account type (#380)
    * add checkbox
    
    * fix is selectable in patch
    
    ---------
    
    Co-authored-by: Fernette Pearl Franco <fernettepearl.franco55@gmail.com>

--- frontend/src/features/registration/hooks/useRegistrationPage.js
+++ frontend/src/features/registration/hooks/useRegistrationPage.js
@@ -38,6 +38,7 @@ export function buildRegistrationRows(registrationConfigs = [], accountTypeOptio
       accountTypeValue: config.accountTypeValue,
       label: matchedOption?.label ?? config.label,
       backendId,
+      isSelectable: config.isSelectable ?? true,
       clientIds,
       clientNames,
       totalClientCount,
@@ -313,6 +314,7 @@ export function useRegistrationPage() {
       await registrationService.updateAccountType({
         accountTypeId: backendId,
         name: accountTypeName,
+        isSelectable: nextConfig.isSelectable,
         clientIds: nextConfig.clientIds,
       });
       await loadRegistrationConfig({ showLoading: false });
